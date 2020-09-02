f840fbd18a52a68a302501033243574ba7f03914
Ryan Wang <hwwangwang@gmail.com>
2020-09-02 15:14:28 +0800

    add prettier

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id='app'>
+  <div id="app">
     <div class="mb-4">hello wolrd</div>
     <a-button @click="click" type="primary">click me</a-button>
-    <img src='~/assets/logo.png' />
-    <div id='nav'>
-      <router-link to='/'> Home</router-link>
-      <router-link to='/about'>about </router-link>
+    <img src="~/assets/logo.png" />
+    <div id="nav">
+      <router-link to="/"> Home</router-link>
+      <router-link to="/about">about </router-link>
     </div>
     <router-view />
   </div>
@@ -14,8 +14,9 @@
 export default {
   methods: {
     click() {
-      console.log('click me')
-    }
+      console.log("click me");
+      [1].map(x => console.log(x));
+    },
   },
 };
 </script>
